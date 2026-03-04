5d8ad84c1091a059bd40d4e937dbd944a99b3f8d
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2026-03-04 19:23:36 +0530

    feat(util): add last-lines extraction helper

--- kernel/util.c
+++ kernel/util.c
@@ -25,3 +25,41 @@ void sanitize_line(char *s)
 	}
 }
 
+
+int extract_last_lines(const char *buf, int len,
+		       char lines[][81], int max_lines, int line_width)
+{
+	int line_no = max_lines - 1;
+	int pos = len - 1;
+
+	while (pos >= 0 && line_no >= 0) {
+		int line_start, line_end, l;
+		int copy_len;
+
+		while (pos >= 0 && buf[pos] == '\n')
+			pos--;
+		if (pos < 0)
+			break;
+
+		line_end = pos + 1;
+
+		while (pos >= 0 && buf[pos] != '\n')
+			pos--;
+		line_start = pos + 1;
+
+		l = line_end - line_start;
+		if (l <= 0)
+			continue;
+
+
+		memset(lines[line_no], ' ', line_width);
+		lines[line_no][line_width] = '\0';
+
+		copy_len = (l < line_width) ? l : line_width;
+		memcpy(lines[line_no], &buf[line_start], copy_len);
+
+		line_no--;
+	}
+
+	return (max_lines - 1) - line_no;
+}
